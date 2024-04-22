1c2b291ab779a4bbbd704e11cbbd9fbcca839d2c
Yakov <58733756+Yakov469@users.noreply.github.com>
2024-04-22 12:04:50 +0300

    Update function pow.js
    Update with -y and other

--- function pow.js
+++ function pow.js
@@ -1,4 +1,18 @@
 function pow(x, y) {
+    
+    if (!Number.isInteger(y)) {
+        return NaN; 
+    }
+    
+    if (y === 0) {
+        return 1;
+    }
+
+    if (y < 0) {
+        x = 1 / x;
+        y = -y;
+    }
+
     let result = 1;
     for (let i = 0; i < y; i++) {
         result *= x;
@@ -6,5 +20,8 @@ function pow(x, y) {
     return result;
 }
 
-// Як будемо використовати:
+// Як будемо використовувати:
+console.log(pow(2, 3)); // Виведе 8
 console.log(pow(4, 3)); // Виведе 64
+console.log(pow(2, -1)); // Виведе 0.5
+console.log(pow(3, 1.5)); // Виведе NaN
